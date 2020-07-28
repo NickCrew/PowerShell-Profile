69b8b626f1083751aedac50af1985f9a83233c2b
nferguson <nferguson@vispero.com>
2020-07-28 17:20:36 -0400

    update

--- Autoload/Functions.ps1
+++ Autoload/Functions.ps1
@@ -470,3 +470,10 @@ function Invoke-FuzzyRgEdit {
     }
 
 }
+
+function Import-GoUtils {
+	$goutils = 'C:\p4\nferguson_NF7590_DevOps_MAIN_8842\GoCD\GoUtils\GoUtils.psd1'
+	if (Test-Path $goutils) {
+		Import-Module $goutils
+	}
+}
