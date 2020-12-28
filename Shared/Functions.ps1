15851f2074afc25ccb3d6ac96350f9e6d7738857
NickCrew <ncf423@gmail.com>
2020-12-28 14:49:40 -0500

    update

--- Shared/Functions.ps1
+++ Shared/Functions.ps1
@@ -1,21 +1,94 @@
 
-function to ($target) { &$target $input }
+function Convert-LineEndings {
+	<#
+	.SYNOPSIS
+		Convert line endings CRLF <==> LF
+	.DESCRIPTION
+		Default behavior converts CRLF to LF
+	.PARAMETER FilePath
+		File on which to perform conversion
+	.PARAMETER Outfile
+		Output converted file contents here.
+		Default behavior overwrites original file.
+	.PARAMETER ToCRLF
+		Convert LF to CRLF
+	#>
+	[CmdletBinding(SupportsShouldProcess)]
+    param (
+        [Parameter(Position = 0, Mandatory)]
+        [ValidateScript({Test-Path $_})]
+        [string]$FilePath,
+        [string]$Outfile,
+		[switch]$ToCRLF
+    )
 
-Set-Alias -Name asadmin -Value Invoke-CommandAsAdmin
-function Invoke-CommandAsAdmin ( $ArgumentList ) {
-	if ($ArgumentList.Count -gt 1) {
-		$ArgumentList = $ArgumentList -join " "
+    if (-not($Outfile)) { $Outfile = $FilePath }
+
+	if ($ToCRLF) {
+		$to = '`r`n'
+		$from = '`n'
 	}
-	Start-Process pwsh.exe -Verb RunAs -ArgumentList "-Command ${ArgumentList}"
+	else {
+		$to = '`n'
+		$from = '`r`n'
+	}
+
+    $new = $contents -replace $from,$to
+    $new | Set-Content $Outfile -Encoding ascii -Force
 }
 
-#region Functions
+Set-Alias -Name to -Value Send-InputTo
+function Send-InputTo ($target) {
+    # Pipe output to args of $target
+    # i.e. gc foo.txt | to gvim
+    & ${target} $input 
+}
+
+Set-Alias -Name asadmin -Value Invoke-CommandAsAdmin
+function Invoke-CommandAsAdmin {
+    <#
+    .SYNOPSIS
+        Converts a command(s) to a scriptblock and executes with 
+        elevated privileges
+    #>
+    param (
+        # Commands to execute in scriptblock (in order)
+        [Parameter(Position = 0)]
+        [string[]]$Command
+    )
+    if ($IsWindows -or $PSVersionTable.Platform -eq 'Win32NT') {
+        if ($Command.Count -gt 1) { $Command = $Command -join ";" }
+        $al = "-Command &{ ${Command} }"
+        $spi = @{
+            FilePath = 'pwsh.exe'
+            Verb = 'RunAs'
+            ArgumentList = $al
+            Wait = $true
+            PassThru = $true
+        }
+        $cmd = Start-Process @spi
+        if ($cmd.ExitCode -ne 0) {
+            $PSCmdlet.ThrowTerminatingError()
+        }
+    }
+}
+
+ 
+Set-Alias -Name ocvi -Value Open-ClipboardContentsInGvim
+function Open-ClipboardContentsInGvim {
+    <#
+    .SYNOPSIS
+        Open the current clipboard contents in GVim. 'ZZ' saves and exits.
+    #>
+    & gvim +pu+ "+$d" +1 "+nnoremap &lt;buffer&gt; ZZ :%y+&lt;CR&gt;ZQ" "+set nomod"
+}
+
 Set-Alias -Name fp4 -Value Invoke-FuzzyP4Client -Force
 function Invoke-FuzzyP4Client {
-<#
-.SYNOPSIS
-    Select a Perforce client using FZF (fuzzy search)
-#>
+    <#
+    .SYNOPSIS
+        Select a Perforce client from list provided by the p4 server using FZF (fuzzy search)
+    #>
     [CmdletBinding()]
     param (
         # P4 Clients Root Directory
@@ -66,8 +139,7 @@ Set-Alias -Name fdrepo -Value Find-GitRepos -Force
 function Find-GitRepos {
     <#
     .SYNOPSIS
-    Locate all the folders containing git repos under a given path
-
+        Find all the folders containing git repos under a given path $Top
     #>
     Param(
         # Directory at which to start the search
@@ -107,53 +179,57 @@ function Find-GitRepos {
 }
 
 function Copy-SshId {
+    <#
+    .SYNOPSIS
+        Copy an SSH public key to a Windows host
+    #>
     [CmdletBinding()]
     param (
-        # Username
+        # Destination Username 
         [Parameter(Position = 0, Mandatory=$true)]
         [string]
         $User,
 
-        # Hostname or IP
+        # Destination Hostname or IP
         [Parameter(Position = 1, Mandatory=$true)]
         [string]
         $Hostname,
 
-        # Path to public key
+        # Public key (source) to copy 
         [Parameter(Position = 2, Mandatory=$false)]
         [string]
         $PubKeyPath = '~/.ssh/id_rsa'
 
     )
-    $ErrorActionPreference = 'Stop'
 
-    if (Test-Path $PubKeyPath) {
-        $spParams = @{
-            FilePath = 'plink.exe'
-            ArgumentList = "${User}@${Hostname} umask 077; test -d .ssh || mkdir .ssh ; cat >> .ssh/authorized_keys"
+    try {
+        if (Test-Path $PubKeyPath) {
+            $spParams = @{
+                FilePath = 'plink.exe'
+                ArgumentList = "${User}@${Hostname} umask 077; test -d .ssh || mkdir .ssh ; cat >> .ssh/authorized_keys"
+            }
+            Get-Content $PubKeyPath | Start-Process @spParams
         }
-        Get-Content $PubKeyPath | Start-Process @spParams
+    }
+    catch {
+        throw $PSItem
     }
 }
 
 function Start-ShellTranscript {
+    <#
+    .SYNOPSIS
+        Start a new transcript in the specified directory for the current shell
+    #>
     [CmdletBinding()]
     param (
         # Log Output Directory
         [Parameter(Position = 0, Mandatory = $false)]
-        [ValidateScript({Test-Path $_})]
         [string]
-        $LogDir
+        $LogDir = (Join-Path (Split-Path $profile) 'Transcripts')
     )
 
-    if ([String]::IsNullOrEmpty($LogDir)) {
-        $LogDir = Join-Path (Split-Path $Profile) 'Transcripts'
-    }
-
-    if (-not (Test-Path $LogDir)) {
-        New-Item $LogDir -ItemType Directory -Force
-    }
-
+    if (-not(Test-Path $LogDir)) { New-Item $LogDir -ItemType Directory }
     $Timestamp = $(get-date -Format "yyyyMMddhhmmss")
     $OutFilePath = Join-Path $LogDir "${Timestamp}.log"
     Write-Verbose "Starting transcript at: ${OutFilePath}"
@@ -166,14 +242,18 @@ function Start-ShellTranscript {
 
 Set-Alias -Name fdproc -Value Find-Process -Force
 function Find-Process {
+    <#
+    .SYNOPSIS
+        Use pattern matching to find a process by name
+    #>
 	param (
-		[Parameter(Position = 0, Mandatory = $true)]
+		[Parameter(Position = 0, Mandatory)]
 		[string]
-		$Name
+		$Pattern
 	)
 
 	$result = Get-Process | Where-Object {
-        $_.Name -match $arg
+        $_.Name -match $Pattern
     }
 
     return $result
@@ -181,19 +261,19 @@ function Find-Process {
 
 
 function rgf ($arg) {
-<#
-.SYNOPSIS
-    Use Ripgrep to search for filenames
-#>
+    <#
+    .SYNOPSIS
+        Use Ripgrep to search for filenames
+    #>
 	rg --files | rg $arg
 }
 
 Set-Alias -Name obliterate -Value Remove-ItemRecursiveForced -Force
 function Remove-ItemRecursiveForced ($arg) {
-<#
-.SYNOPSIS
-    Recursively and forcefully removing all sub-directories and files under $arg
-#>
+    <#
+    .SYNOPSIS
+        Recursively and forcefully removing all sub-directories and files under $arg
+    #>
 	$p = @{
 		Path    = $arg
 		Force   = $true
@@ -202,7 +282,6 @@ function Remove-ItemRecursiveForced ($arg) {
 	}
 	Remove-Item @p
 }
-
 
 
 Set-Alias -Name frg -Value Invoke-FuzzyRgEdit -Force
@@ -214,34 +293,24 @@ function Invoke-FuzzyRgEdit {
     [CmdletBinding()]
     param (
         # Pattern
-        [Parameter(Position = 0, Mandatory = $true)]
+        [Parameter(Position = 0, Mandatory)]
         [string]
         $Pattern,
 
         # Editor
-        [Parameter(Position = 1, Mandatory = $false)]
+        [Parameter()]
         [string]
-        $Editor = $Env:Editor,
+        $Editor = (& { if ($Env:Editor) { $Env:Editor } else { 'gvim' } }),
 
         # Search file names
+        [Parameter()]
         [switch]
         $Files = $false
     )
 
-    if ([String]::IsNullOrEmpty($Editor)) {
-        $Editor = 'gvim'
-    }
-
-    if ($Files) {
-        &${Editor} $(rgf $Pattern | fzf)
-    }
-    else {
-        &${Editor} $((rg $Pattern | fzf).Split(":")[0])
-    }
-
+    if ($Files) { & ${Editor} $(rgf $Pattern | fzf) }
+    else { & ${Editor} $((rg $Pattern | fzf).Split(":")[0]) }
 }
-#endregion
 
-#region Aliases
+# Other Aliases
 Set-Alias -Name sclip -Value Set-Clipboard -Force
-#endregion
--- profile.ps1
+++ profile.ps1
@@ -1,11 +1,13 @@
+######################################################
 # Name: profile.ps1
 # Profile: CurrentUserAllHosts
 # Loads after: AllUsers, AllHosts
 # Loads before: CurrentUserCurrentHost
 # PSVersion: 7
+######################################################
 
 # PSReadline
-Set-PSReadlineOption -EditMode vi -BellStyle None
+Set-PSReadlineOption -EditMode vi 
 Set-PSReadlineOption -BellStyle None
 Set-PSReadlineKeyHandler -Chord Ctrl+Alt+s -Function SwapCharacters
 Set-PSReadlineKeyHandler -Key Alt+r -Function ViSearchHistoryBackward
@@ -15,3 +17,9 @@ Set-PSReadlineKeyHandler -Key Tab -Function MenuComplete
 TabExpansion2.ps1
 Invoke-Build.ArgumentCompleters.ps1
 gci "${PSScriptRoot}/Shared/Completions" -Filter *.ps1 -Recurse | % { . $_.FullName }
+
+#region conda initialize
+# !! Contents within this block are managed by 'conda init' !!
+(& "C:\Users\ncf42\anaconda3\Scripts\conda.exe" "shell.powershell" "hook") | Out-String | Invoke-Expression
+#endregion
+
